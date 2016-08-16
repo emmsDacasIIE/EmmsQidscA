323c0c8502adc152aeb4124fa6ebae0cae1a7bbd
Sun Rx <sunrongxin@iie.ac.cn>
2016-08-16 15:04:54 +0800

    EMM QiDi in AS Add GetWebServerAddr With Auth Prot in UrlManage.java

--- msp/src/main/java/cn/qdsc/msp/manager/UrlManager.java
+++ msp/src/main/java/cn/qdsc/msp/manager/UrlManager.java
@@ -2,20 +2,25 @@ package cn.qdsc.msp.manager;
 
 /**
  * Created by Sun RX on 2016-8-16.
+ * UrlManager to bulid Url with https/http and Ip address
  */
 
 public class UrlManager {
     static int HTTPS = 0;
     static int HTTP = 1;
     static String[] protType= {"https://","http://"};
-    static String getWebServeUrl(int type){
+    static String getWebServerUrl(int type){
         if(type> protType.length-1)
             type = 0;
 
         return protType[type]+AddressManager.getAddrWebservice();
     }
 
-    static String getWebServeUrl(){
+    static String getWebServerUrl(){
+        return protType[0]+AddressManager.getAddrWebservice();
+    }
+
+    static String getWebServerUrlWithAuthPort(){
         return protType[0]+AddressManager.getAddrWebservice();
     }
 }
